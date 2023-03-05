2d6d84a944fc7ffbe09e73fb080c59666bb5c5da
Stepan Ustimenko <50939552+Apochromat@users.noreply.github.com>
2023-03-05 16:46:56 +0700

    feat(account): добавил модели сущностей для профиля пользователя

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using social_network_API.Models;
+
+namespace social_network_API;
+
+public class ApplicationDbContext : IdentityDbContext<User, Role, Guid, IdentityUserClaim<Guid>, UserRole,
+    IdentityUserLogin<Guid>, IdentityRoleClaim<Guid>, IdentityUserToken<Guid>> {
+    private readonly IConfiguration _configuration;
+
+    public ApplicationDbContext(IConfiguration configuration) : base() {
+        _configuration = configuration;
+    }
+
+    public DbSet<User> Users { get; set; }
+    public override DbSet<Role> Roles { get; set; }
+    public DbSet<UserRole> UserRoles { get; set; }
+    public DbSet<Location> Locations { get; set; }
+    public DbSet<Job> Jobs { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<User>(e => { e.ToTable("Users"); });
+        modelBuilder.Entity<Role>(e => { e.ToTable("Roles"); });
+        modelBuilder.Entity<UserRole>(e => {
+            e.ToTable("UserRoles");
+            e.HasOne(x => x.Role)
+                .WithMany(x => x.Users)
+                .HasForeignKey(x => x.RoleId)
+                .OnDelete(DeleteBehavior.Cascade);
+            e.HasOne(x => x.User)
+                .WithMany(x => x.Roles)
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+        /*modelBuilder.Entity<Classroom>().HasKey(x => x.Id);
+        modelBuilder.Entity<Group>().HasKey(x => x.Id);*/
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        optionsBuilder.UseMySql(_configuration.GetConnectionString("MySQLDatabase"),
+            new MySqlServerVersion(new Version(8, 0, 31)));
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
+using System.Reflection;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using social_network_API;
+using social_network_API.Models;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Caching.Distributed;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add CORS Policy
@@ -10,14 +27,73 @@ builder.Services.AddCors(options => {
         });
 });
 
-// Add services to the container.
+// Add services to the container. Add ability to right convert strings to enums
+builder.Services.AddControllers().AddJsonOptions(opts => {
+    var enumConverter = new JsonStringEnumConverter();
+    opts.JsonSerializerOptions.Converters.Add(enumConverter);
+});
 
-builder.Services.AddControllers();
+// Connect DB
+builder.Services.AddDbContext<ApplicationDbContext>();
+
+// Add Services
+
+// Add Identity
+builder.Services.AddIdentity<User, Role>(options => { options.SignIn.RequireConfirmedEmail = false; })
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders()
+    .AddSignInManager<SignInManager<User>>()
+    .AddUserManager<UserManager<User>>()
+    .AddRoleManager<RoleManager<Role>>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option => {
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Social Network API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] { }
+        }
+    });
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    option.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+builder.Services.AddAuthorization();
+// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//     .AddJwtBearer(options => {
+//         options.TokenValidationParameters = new TokenValidationParameters {
+//             ValidateIssuer = true,
+//             ValidIssuer = JwtConfiguration.Issuer,
+//             ValidateAudience = true,
+//             ValidAudience = JwtConfiguration.Audience,
+//             ValidateLifetime = true,
+//             IssuerSigningKey = JwtConfiguration.GetSymmetricSecurityKey(),
+//             ValidateIssuerSigningKey = true,
+//         };
+//     });
 
 var app = builder.Build();
+
+// auto migration
+using var serviceScope = app.Services.CreateScope();
+var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+context.Database.Migrate();
+await app.ConfigureIdentityAsync();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
